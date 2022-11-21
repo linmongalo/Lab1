c293bda622b913ee24a81b08570bc9984f9d36f7
linmongalo <72529541+linmongalo@users.noreply.github.com>
2022-11-21 00:35:33 -0500

    Add files via upload

---
+++ lab2_.py
@@ -0,0 +1,31 @@
+#Smart Robot: Object Oriented Design
+
+
+def kivaRobot(self, initiate): #The Robot is idle and initiates
+    if initiate == 0:
+        self._idle()
+        self._powerOn()
+    
+elif stateno == 1: #The robot begins to move 
+        def wheels(self):
+     self.turnLeft()
+    self.spin()
+
+elif stateno == 2: #The robot detects an object in the way
+        def motionDetected(self):
+     self._pirSensor(on)
+    self._pressureSensor(on)
+    self._IRsensorOn(on)
+
+def motionDetected(self):
+    print("PIRSensor detects detects motion")
+    self.singleOn(1)
+
+def objectDetected(self):
+    print("Pressure sensor on bumper detects object")
+    self.singleOn(1)
+
+def objectDetected(self):
+    print("IR sensor detects object")
+    self.singleOn(1)
+
